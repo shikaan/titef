888d93b08470d031231857a1413d8cf07e856dab
shikaan <spagnolo.manu@gmail.com>
2019-10-04 21:33:41 +0200

    Add types

---
+++ dist/types.d.ts
@@ -0,0 +1,8 @@
+declare var xit: (name: string, callback: Function) => void;
+declare var it: (name: string, callback: Function) => void;
+declare var describe: (name: string, callback: Function) => void;
+declare var test: (name: string, callback: Function) => void;
+declare var xtest: (name: string, callback: Function) => void;
+declare var suite: (name: string, callback: Function) => void;
+declare var spec: (name: string, callback: Function) => void;
+declare var xspec: (name: string, callback: Function) => void;
