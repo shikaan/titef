a703aba3ee04199c3500821102a968b6f3b0f5cc
manuel <spagnolo.manu@gmail.com>
2020-02-13 11:23:57 +0100

    feat: make declarations work with global functions

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,5 +1,8 @@
-declare namespace Titef {
-  interface SuiteOptions {
+// Type definitions for titef
+// Definitions by: Manuel Spagnolo <spagnolo.manu@gmail.com>
+
+declare namespace titef {
+  export interface SuiteOptions {
     silent?: boolean,
     setup?: () => void,
     teardown?: () => void,
@@ -7,22 +10,35 @@ declare namespace Titef {
     eachTeardown?: () => void
   }
 
-  var xit: (name: string, callback: Function) => void;
-  var it: (name: string, callback: Function) => void;
-  var test: (name: string, callback: Function) => void;
-  var xtest: (name: string, callback: Function) => void;
-  var spec: (name: string, callback: Function) => void;
-  var xspec: (name: string, callback: Function) => void;
-
-  var suite: {
-    (name: string, callback: Function): void;
-    (name: string, options: SuiteOptions, callback: Function): void;
+  interface SpecFunction {
+    (name: string, callback?: Function): void;
   }
 
-  var describe: {
-    (name: string, callback: Function): void;
-    (name: string, options: SuiteOptions, callback: Function): void;
+  interface SuiteFunction {
+    (name: string, options: SuiteOptions, callback?: Function): void;
+
+    (name: string, callback?: Function): void;
   }
+
+  declare var describe: titef.SuiteFunction;
+  declare var suite: titef.SuiteFunction;
+
+  declare var xit: titef.SpecFunction;
+  declare var it: titef.SpecFunction;
+  declare var test: titef.SpecFunction;
+  declare var xtest: titef.SpecFunction;
+  declare var spec: titef.SpecFunction;
+  declare var xspec: titef.SpecFunction;
 }
 
-export = Titef;
+declare var describe: titef.SuiteFunction;
+declare var suite: titef.SuiteFunction;
+
+declare var xit: titef.SpecFunction;
+declare var it: titef.SpecFunction;
+declare var test: titef.SpecFunction;
+declare var xtest: titef.SpecFunction;
+declare var spec: titef.SpecFunction;
+declare var xspec: titef.SpecFunction;
+
+export = titef;
